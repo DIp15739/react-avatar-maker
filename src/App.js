8a060ce25db2659b97fad66fc7903bb47fb2c176
Dip Vachhani <dv.dipvachhani@gmail.com>
2021-09-17 12:10:39 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import html2canvas from "html2canvas";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import "./assets/scss/style.scss";
 
@@ -7,25 +7,38 @@ function toBase64(data) {
   return "data:image/png;base64," + new Buffer.from(data).toString("base64");
 }
 
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function App() {
   const [dataList, setDataList] = useState({});
   const [dressupState, setDressupState] = useState({});
   const [msg, setMsg] = useState();
   const [upAva, setUpAva] = useState(false);
+  const [teamName, setTeamName] = useState();
+  const [teamRating, setTeamRating] = useState();
+  const inputTeamName = useRef(null);
 
+  function handleTeamName() {
+    setTeamName(inputTeamName.current.value);
+  }
   useEffect(() => {
     fetch("http://localhost:3003/user/avatar-data")
       .then((response) => response.json())
       .then((data) => {
         setDataList(data.message);
         setDressupState({
-          eyes: data.message.eyes[0],
-          ears: data.message.ears[0],
-          mouth: data.message.mouth[0],
-          nose: data.message.nose[0],
-          clothes: data.message.clothes[0],
+          bg: data.message.bg[0],
+          helement: data.message.helement[0],
+          logo: data.message.logo[0],
+          city: data.message.city[0],
+          tck: data.message.tck[0],
         });
       });
+    setTeamRating(randomIntFromInterval(10, 99));
+    setTeamName("Team Name");
   }, []);
 
   function handleClick(item, new_current) {
@@ -48,6 +61,9 @@ function App() {
       ...dressupState,
       ...newState,
     });
+
+    setTeamRating(randomIntFromInterval(10, 99));
+    setTeamName("Team Name");
   }
 
   async function saveAvatar() {
@@ -62,13 +78,15 @@ function App() {
           //add blob in db
 
           const formdata = new FormData();
-          formdata.append("userName", "t1");
+          formdata.append("userName", "t2");
           formdata.append("img", canvasBlob);
-          formdata.append("eyes", dressupState.eyes.id);
-          formdata.append("ears", dressupState.ears.id);
-          formdata.append("mouth", dressupState.mouth.id);
-          formdata.append("nose", dressupState.nose.id);
-          formdata.append("clothes", dressupState.clothes.id);
+          formdata.append("bg", dressupState.bg.id);
+          formdata.append("helement", dressupState.helement.id);
+          formdata.append("logo", dressupState.logo.id);
+          formdata.append("city", dressupState.city.id);
+          formdata.append("tck", dressupState.tck.id);
+          formdata.append("teamName", teamName);
+          formdata.append("teamRating", teamRating);
 
           fetch("http://localhost:3003/user/create-avatar", {
             method: "POST",
@@ -95,13 +113,18 @@ function App() {
     await fetch(`http://localhost:3003/user/get-avatar/${username}`)
       .then((response) => response.json())
       .then((data) => {
+        console.log(data);
         const getAvatarData = {};
         Object.keys(dataList).map((x) => {
-          if (x === "body") return;
+          if (x === "teamName") return;
+          if (x === "teamRating") return;
           const list = dataList[x].find((e) => e.id === data.data[x]);
           getAvatarData[x] = list;
         });
         setDressupState(getAvatarData);
+        setTeamName(data.data.teamName);
+        setTeamRating(data.data.teamRating);
+        setMsg("user name: " + " " + data.data.userName);
         setUpAva(true);
       });
   }
@@ -119,11 +142,13 @@ function App() {
           const formdata = new FormData();
           formdata.append("userName", "t1");
           formdata.append("img", canvasBlob);
-          formdata.append("eyes", dressupState.eyes.id);
-          formdata.append("ears", dressupState.ears.id);
-          formdata.append("mouth", dressupState.mouth.id);
-          formdata.append("nose", dressupState.nose.id);
-          formdata.append("clothes", dressupState.clothes.id);
+          formdata.append("bg", dressupState.bg.id);
+          formdata.append("helement", dressupState.helement.id);
+          formdata.append("logo", dressupState.logo.id);
+          formdata.append("city", dressupState.city.id);
+          formdata.append("tck", dressupState.tck.id);
+          formdata.append("teamName", teamName);
+          formdata.append("teamRating", teamRating);
 
           fetch("http://localhost:3003/user/update-avatar", {
             method: "POST",
@@ -150,11 +175,11 @@ function App() {
     <div className="App">
       <div className="row">
         <div className="col" id="captureAvatar">
-          <img
+          {/* <img
             src={dataList.body && toBase64(dataList.body[0].url.data)}
             alt="body"
             id="body"
-          />
+          /> */}
           {Object.keys(dressupState).map((item) => (
             <div className={`${item}-container`}>
               <img
@@ -165,11 +190,12 @@ function App() {
               />
             </div>
           ))}
+          <h1 id="teamName">{teamName}</h1>
+          <h1 id="teamRating">{teamRating}</h1>
         </div>
         <div className="col">
           <div>
             {Object.keys(dataList).map((item) => {
-              if (item === "body") return;
               return (
                 <>
                   <h2>{item}</h2>
@@ -187,16 +213,23 @@ function App() {
               );
             })}
           </div>
-          <input
-            type="button"
-            value="RANDOMIZE"
-            id="randomize"
-            onClick={() => randomize()}
-          />
-          {!upAva && <button onClick={saveAvatar}>Save Avatar</button>}
-          <button onClick={getAvatar}>Get Avatar</button>
-          {upAva && <button onClick={updateAvatar}>Update Avatar</button>}
-          {msg && <h2>{msg}</h2>}
+          <div className="buttons">
+            <label htmlFor="teamName">Enter Team name</label>
+            <input
+              type="text"
+              value={teamName}
+              ref={inputTeamName}
+              onChange={handleTeamName}
+            />
+            <br />
+            <button id="randomize" onClick={() => randomize()}>
+              randomize
+            </button>
+            {!upAva && <button onClick={saveAvatar}>Save Avatar</button>}
+            <button onClick={getAvatar}>Get Avatar</button>
+            {upAva && <button onClick={updateAvatar}>Update Avatar</button>}
+            {msg && <h2>{msg}</h2>}
+          </div>
         </div>
       </div>
       {/* <div id="imagePpreview">
